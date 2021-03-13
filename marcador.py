47667936509cf989824e976119bc81c9d28482f8
yailm07-23 <79061359+yailm07-23@users.noreply.github.com>
2021-03-13 18:18:28 +0100

    añadir torpedo y su funcionalidad

--- marcador.py
+++ marcador.py
@@ -1,4 +1,8 @@
 from time import time
+from tkinter import font
+
+import configuracion
+
 
 
 class Marcador:
@@ -6,6 +10,7 @@ class Marcador:
         self.canvas = canvas
         self.puntos = 0
         self.tiempo_fin = time() + 30
+        self.hundido = False
         canvas.create_text(50, 30, text="TIEMPO", fill="white")
         canvas.create_text(150, 30, text="PUNTOS", fill="white")
         self.texto_tiempo = canvas.create_text(50, 50, fill="white")
@@ -13,12 +18,17 @@ class Marcador:
         self.actualizar()
 
     def actualizar(self):
-        tiempo_restante = self.tiempo_fin - time()
-        if tiempo_restante < 0:
-            self.canvas.itemconfig(self.texto_puntos, text="HAS PERDIDO")
+        if self.has_perdido():
+            FUENTE_TITULO = font.Font(family='Helvetica', size=36, weight='bold')
+            self.canvas.create_text(configuracion.CENTRO_X, configuracion.CENTRO_Y, text="¡HAS PERDIDO!", fill="white",
+                                    font=FUENTE_TITULO)
             return
+        tiempo_restante = self.tiempo_fin - time()
         self.canvas.itemconfig(self.texto_tiempo, text=str(int(tiempo_restante)))
         self.canvas.itemconfig(self.texto_puntos, text=str(self.puntos))
 
-    def tiempo_agotado(self):
-        return time() > self.tiempo_fin
+    def has_perdido(self):
+        return time() > self.tiempo_fin or self.hundido
+
+    def registrar_impacto_con_torpedo(self):
+        self.hundido = True
---
+++ torpedo.py
@@ -0,0 +1,46 @@
+from random import randint
+
+from configuracion import CANVAS_ANCHURA, CANVAS_ALTURA
+
+TORPEDO_RADIO = 10
+MAX_TORPEDO_VELOCIDAD = 10
+TORPEDO_COLOR = 'red'
+TORPEDO_PROBABILIDAD = 10
+MARGEN = TORPEDO_RADIO
+TORPEDO_X_INICIAL = CANVAS_ANCHURA + MARGEN
+
+
+class Torpedo:
+    def __init__(self, canvas):
+        print('creando torpedo')
+        self.canvas = canvas
+        self.x = TORPEDO_X_INICIAL
+        self.y = randint(0, CANVAS_ALTURA)
+        self.radio = TORPEDO_RADIO
+        self.circulo = canvas.create_oval(self.x - self.radio, self.y - self.radio,
+                                          self.x + self.radio, self.y + self.radio,
+                                          fill=TORPEDO_COLOR)
+        self.velocidad = randint(1, MAX_TORPEDO_VELOCIDAD)
+        self.activo = True
+
+    def mover(self):
+        if not self.activo:
+            if randint(1, TORPEDO_PROBABILIDAD) == 1:
+                # Regenarar torpedo
+                self.x = TORPEDO_X_INICIAL
+                self.y = randint(0, CANVAS_ALTURA)
+                self.activo = True
+            else:
+                return
+        self.x -= self.velocidad
+        self.canvas.moveto(self.circulo, self.x, self.y)
+        if self.x < 0 - TORPEDO_RADIO * 2:
+            # El torpedo se desactiva cuando se sale completamente de la pantalla
+            self.desactivar()
+
+    def desactivar(self):
+        print('desactivando torpedo')
+        self.activo = False
+
+    def detonar(self):
+        self.desactivar()
