be581607ccb832a0faa86ce26c4936f7211c03d2
yailm07-23 <79061359+yailm07-23@users.noreply.github.com>
2021-04-26 20:09:22 +0200

    Mostrar números de nivel estáticos

--- menu.py
+++ menu.py
@@ -13,25 +13,28 @@ SALIR_Y = 350
 INICIAR_Y = 450
 
 
+
 class Menu:
     def __init__(self, ventana, canvas):
+        self.FUENTE_TITULO = font.Font(family='Helvetica', size=36, weight='bold')
+        self.FUENTE_NORMAL = font.Font(family='Helvetica', size=20, weight='bold')
         self.ventana = ventana
         self.canvas = canvas
         self.activo = True
         self.objetos_canvas = list()
 
-        FUENTE_TITULO = font.Font(family='Helvetica', size=36, weight='bold')
-        FUENTE_NORMAL = font.Font(family='Helvetica', size=20, weight='bold')
+
 
         self.objetos_canvas.append(
             self.canvas.create_text(configuracion.CENTRO_X, TITULO_Y, text=TEXTO_TITULO, fill="white",
-                                    font=FUENTE_TITULO))
+                                    font=self.FUENTE_TITULO))
         self.objetos_canvas.append(
             self.canvas.create_text(configuracion.CENTRO_X, NIVEL_Y, text=TEXTO_NIVEL, fill="white",
-                                    font=FUENTE_NORMAL))
+                                    font=self.FUENTE_NORMAL))
+        self.mostrar_nivel(1)
         self.objetos_canvas.append(
             self.canvas.create_text(configuracion.CENTRO_X, INICIAR_Y, text=TEXTO_INICIAR, fill="white",
-                                    font=FUENTE_NORMAL))
+                                    font=self.FUENTE_NORMAL))
 
         f = open("record.txt", "r")
         linea = f.readline()
@@ -42,7 +45,7 @@ class Menu:
         texto_record = f"Record: {maxima_puntuacion}"
         self.objetos_canvas.append(
             self.canvas.create_text(configuracion.CENTRO_X, RECORD_Y, text=texto_record, fill="white",
-                                    font=FUENTE_NORMAL))
+                                    font=self.FUENTE_NORMAL))
 
     def seleccionar_nivel(self, nivel):
         pass
@@ -65,3 +68,14 @@ class Menu:
         if evento.keysym == '2':
             self.seleccionar_nivel(2)
             return True
+
+    def mostrar_nivel(self, nivel):
+        self.objetos_canvas.append(
+            self.canvas.create_text(configuracion.CENTRO_X + 150, NIVEL_Y, text="1", fill="white",
+                                    font=self.FUENTE_NORMAL))
+        self.objetos_canvas.append(
+            self.canvas.create_text(configuracion.CENTRO_X + 180, NIVEL_Y, text="2", fill="white",
+                                    font=self.FUENTE_NORMAL))
+        self.objetos_canvas.append(
+            self.canvas.create_text(configuracion.CENTRO_X + 210, NIVEL_Y, text="3", fill="white",
+                                    font=self.FUENTE_NORMAL))
